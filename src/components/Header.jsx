70ee26030ca8c61e72473d7920ff641df324cc6a
Mohieb <dev.mohieb@gmail.com>
2023-05-28 18:12:58 +0300

    adjust spacing in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="mb-9 max-w-[280px] text-center">
-      <h1 className=" text-4xl">Tenzies</h1>
+      <h1 className=" text-4xl mb-4">Tenzies</h1>
       <p className="font-Inter text-sm md:text-base font-normal text-[#4A4E74]">
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
